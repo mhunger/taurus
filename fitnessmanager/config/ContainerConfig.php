0746f54c17269a2c7b8df90369d7104b0670fb79
mhunger <m.hunger@yahoo.de>
2016-12-16 10:59:08 +0100

    Moved controller to its app
    - created app fitnessmanager in top-level and created workout controller
    - moved tests inside framework

---
+++ fitnessmanager/config/ContainerConfig.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: michaelhunger
+ * Date: 16/12/16
+ * Time: 10:39
+ */
+namespace fitnessmanager\config;
+
+class ContainerConfig {
+    const SERVICE_REQUEST = 'Request';
+    const SERVICE_WORKOUT_CONTROLLER = 'WorkoutController';
+}
