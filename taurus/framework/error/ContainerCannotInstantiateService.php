3a038466c35ae654ce1eecde64df71f4042647ae
mhunger <m.hunger@yahoo.de>
2016-12-21 21:35:36 +0100

    Create exception for case, when service cannot be instantiated
    - exception to signify state, when a service cannot be instantiated e.g. because parameters are wrongly marshalled

---
+++ taurus/framework/error/ContainerCannotInstantiateService.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: michaelhunger
+ * Date: 19/12/16
+ * Time: 20:32
+ */
+
+namespace taurus\framework\error;
+
+
+class ContainerCannotInstantiateService extends \Exception {}
