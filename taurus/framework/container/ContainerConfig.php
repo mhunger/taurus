c32b3d608a429ec8c5cbda8bde4495c823154dd6
mhunger <m.hunger@yahoo.de>
2016-12-22 10:30:16 +0100

    Move service name constants to config implementations rather than interface
    - moved the taurus service name definitions to the class from the ContainerConfig interface

--- taurus/framework/container/ContainerConfig.php
+++ taurus/framework/container/ContainerConfig.php
@@ -12,12 +12,35 @@ use taurus\framework\routing\RouteConfig;
 use taurus\framework\routing\Request;
 use taurus\framework\mock\MockServer;
 
+/**
+ * Interface to implement for different configurations in different contexts, environments. Should enable to savely extend Configuration
+ * in different situations without having to touch the core or app config
+ *
+ * Interface ContainerConfig
+ * @package taurus\framework\container
+ */
 interface ContainerConfig {
-    const SERVICE_ROUTE_CONFIG = RouteConfig::class;
-    const SERVICE_REQUEST = Request::class;
-    const SERVICE_MOCK_SERVER = MockServer::class;
 
+    /**
+     * Return the service definition for the service with name $name. Currently name is the full clath path with namespace
+     * @param $name
+     * @return mixed
+     */
     public function getServiceConfigByName($name);
+
+    /**
+     * Merge two configurations into one. The configuration passed will be merged into the one on which the method is called. This is done
+     * by merging the service definitions array inside the objects. Service defined in both will be unified
+     *
+     * @param ContainerConfig $containerConfig
+     * @return mixed
+     */
     public function merge(ContainerConfig $containerConfig);
+
+    /**
+     * Return the service definitions for this configuration as array
+     *
+     * @return mixed
+     */
     public function getServiceDefinitions();
 }
--- taurus/framework/container/TaurusContainerConfig.php
+++ taurus/framework/container/TaurusContainerConfig.php
@@ -8,6 +8,10 @@
 
 namespace taurus\framework\container;
 
+use taurus\framework\mock\MockServer;
+use taurus\framework\routing\Request;
+use taurus\framework\routing\RouteConfig;
+
 /**
  * Standard taurus container config contains all the services used inside taurus
  *
@@ -15,6 +19,10 @@ namespace taurus\framework\container;
  * @package taurus\framework\container
  */
 class TaurusContainerConfig extends AbstractContainerConfig {
+
+    const SERVICE_ROUTE_CONFIG = RouteConfig::class;
+    const SERVICE_REQUEST = Request::class;
+    const SERVICE_MOCK_SERVER = MockServer::class;
 
     public function __construct() {
         $this->serviceDefinitions[self::SERVICE_ROUTE_CONFIG] =
