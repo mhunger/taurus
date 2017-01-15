535029314f60c38dd67e148f8037d48f3b44037c
mhunger <m.hunger@yahoo.de>
2017-01-15 20:52:44 +0100

    Add database manager services to test config
    - added mysql connection to test container config
    - added database manager to test container config

--- taurus/tests/fixtures/TestContainerConfig.php
+++ taurus/tests/fixtures/TestContainerConfig.php
@@ -12,6 +12,8 @@ namespace taurus\tests\fixtures;
 use taurus\framework\container\AbstractContainerConfig;
 use taurus\framework\container\ServiceConfig;
 use taurus\framework\container\TaurusContainerConfig;
+use taurus\framework\db\DatabaseManager;
+use taurus\framework\db\MySqlConnection;
 use taurus\framework\Environment;
 use taurus\tests\fixtures\LoadDependenciesForLiteralsTestClass;
 
@@ -20,6 +22,8 @@ class TestContainerConfig extends AbstractContainerConfig {
     const SERVICE_TEST_LITERALS = LoadDependenciesForLiteralsTestClass::class;
     const SERVICE_TEST_LITERALS_IN_DEPENDENCY = LoadDependenciesWithParamsInDependency::class;
     const SERVICE_ENVIRONMENT = Environment::class;
+    const SERVICE_MYSQL_CONNECTION = MySqlConnection::class;
+    const SERVICE_DB_MANAGER = DatabaseManager::class;
 
     /**
      * Method to define the ServicConfig objects for the config class
@@ -48,6 +52,18 @@ class TestContainerConfig extends AbstractContainerConfig {
             new ServiceConfig(self::SERVICE_ENVIRONMENT,
                 'environment',
                 [Environment::TEST]
+            );
+
+        $this->serviceDefinitions[self::SERVICE_MYSQL_CONNECTION] =
+            new ServiceConfig(self::SERVICE_MYSQL_CONNECTION,
+                'MysqlConnection',
+                ['localhost', 'taurus', 'taurus', 'taurus']
+            );
+
+        $this->serviceDefinitions[self::SERVICE_DB_MANAGER] =
+            new ServiceConfig(self::SERVICE_DB_MANAGER,
+                'dbManager',
+                []
             );
     }
 }
