94cecd5b36f296b04d13007c69ed0384d46e312d
mhunger <m.hunger@yahoo.de>
2017-02-04 19:52:07 +0100

    Add interface for entity meta data
    - added an interface for entity meta data class, because some entities for different storages might have different structure of meta data

---
+++ taurus/framework/db/EntityMetaDataWrapper.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: michaelhunger
+ * Date: 29/01/17
+ * Time: 20:22
+ */
+
+namespace taurus\framework\db;
+
+
+interface EntityMetaDataWrapper
+{
+
+    public function getIdField($class);
+
+    public function getTable($class);
+}
