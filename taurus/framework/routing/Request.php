a8bf5e825cbe2051e1dd8ac510590b416597da2b
mhunger <m.hunger@yahoo.de>
2017-02-04 21:39:07 +0100

    Extend Request to be able to support url parameters
    - added variables to store request parameter array from $_REQUEST
    - added method to parse the request parameters into that
    - storing global array locally now to prevent changes later
    - provide method to retrieve all parameters or paramter by name

--- taurus/framework/routing/Request.php
+++ taurus/framework/routing/Request.php
@@ -10,21 +10,74 @@ namespace taurus\framework\routing;
 
 
 class Request {
+    /**
+     * @var
+     */
     protected $url;
 
+    /**
+     * @var
+     */
     protected $method;
 
+    /**
+     * @var
+     */
+    protected $requestVariables;
+
+    /**
+     *
+     */
     public function __construct() {
+        $this->server = $_SERVER;
+        $this->request = $_REQUEST;
+
         $this->url = $this->parseUrl();
         $this->method = $this->parseMethod();
+        $this->parseRequestParams();
     }
 
+    /**
+     *
+     */
+    private function parseRequestParams()
+    {
+        foreach ($_REQUEST as $name => $value) {
+            $this->requestVariables[$name] = $value;
+        }
+    }
+
+    /**
+     * @param $name
+     * @return mixed
+     */
+    public function getParamByName($name)
+    {
+        if (isset($this->requestVariables[$name])) {
+            return $this->requestVariables[$name];
+        }
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAllParams()
+    {
+        return $this->requestVariables;
+    }
+
+    /**
+     * @return mixed
+     */
     public function parseUrl() {
-        return $_SERVER['REQUEST_URI'];
+        return parse_url($this->server['REQUEST_URI'], PHP_URL_PATH);
     }
 
+    /**
+     * @return mixed
+     */
     public function parseMethod() {
-        return $_SERVER['REQUEST_METHOD'];
+        return $this->server['REQUEST_METHOD'];
     }
 
     /**
