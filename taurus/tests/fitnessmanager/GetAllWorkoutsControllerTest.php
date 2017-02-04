f307bcb48baff9febce6bb3f709f85e81fe52060
mhunger <m.hunger@yahoo.de>
2017-02-04 22:26:19 +0100

    Add test for get all workouts controller
    - added controller test that tests that all items are returned

---
+++ taurus/tests/fitnessmanager/GetAllWorkoutsControllerTest.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: michaelhunger
+ * Date: 04/02/17
+ * Time: 22:20
+ */
+
+namespace taurus\tests\fitnessmanager;
+
+
+use PHPUnit\Framework\TestCase;
+use taurus\framework\Container;
+use taurus\framework\container\TaurusContainerConfig;
+use taurus\framework\Http\HttpJsonResponse;
+use taurus\tests\fixtures\TestContainerConfig;
+use taurus\framework\mock\MockServer;
+
+/**
+ * Class GetAllWorkoutsControllerTest
+ * @package taurus\tests\fitnessmanager
+ */
+class GetAllWorkoutsControllerTest extends TestCase
+{
+    /**
+     *
+     */
+    public function setUp()
+    {
+
+    }
+
+    /**
+     * @throws \taurus\framework\error\ContainerCannotInstantiateService
+     */
+    public function testGetMethod()
+    {
+        /** @var MockServer $mockServer */
+        $mockServer = Container::getInstance()->setContainerConfig(
+            new TestContainerConfig()
+        )->getService(TaurusContainerConfig::SERVICE_MOCK_SERVER);
+        $actualResponse = $mockServer->get(
+            '/api/items',
+            'GET',
+            []
+        );
+
+        $json = '[{"id":"1","date":"2012-01-01 12:00:00"},{"id":"2","date":"2015-01-01 04:00:00"}]';
+        $responseObj = json_decode($json);
+
+        $expectedResponse = (new HttpJsonResponse(201, $responseObj))->getJson();
+
+        $this->assertEquals(
+            $expectedResponse,
+            $actualResponse,
+            "Controller to get all workouts did not return correct result"
+        );
+    }
+}
