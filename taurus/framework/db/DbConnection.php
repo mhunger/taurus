1d2996a1b1b83c7a8a3dcc24b48c8f464637d0ed
mhunger <m.hunger@yahoo.de>
2017-02-05 16:45:03 +0100

    Adding the insert method to db connection interface
    - added the insert method to db connection interface, in order for later storage layers to be forced to implement that too

--- taurus/framework/db/DbConnection.php
+++ taurus/framework/db/DbConnection.php
@@ -11,11 +11,39 @@ namespace taurus\framework\db;
 
 use taurus\framework\db\query\Query;
 
+/**
+ * Abstraction for any db interaction. It should be implemented by specifc storage layer classes that will adapt the
+ * specific mechanics of that storage. the queries should be build with special query builder classes
+ * that take in QAL objects and build query strings from it
+ *
+ * Interface DbConnection
+ * @package taurus\framework\db
+ */
 interface DbConnection {
 
+    /**
+     * @param $sql
+     * @param null $class
+     * @return mixed
+     */
     public function fetchObjects($sql, $class = null);
 
+    /**
+     * @param $sql
+     * @return mixed
+     */
     public function executeRaw($sql);
 
+    /**
+     * @param Query $query
+     * @param null $class
+     * @return mixed
+     */
     public function execute(Query $query, $class = null);
+
+    /**
+     * @param Query $query
+     * @return mixed
+     */
+    public function insert(Query $query);
 }
