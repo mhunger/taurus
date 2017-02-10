cc8f9d274f36c2a53294dbc490687261b4d0594b
mhunger <m.hunger@yahoo.de>
2017-02-10 22:28:37 +0100

    Add method to boot
    - add method set environment that will set global environment for application
    - added method boot config to merge base taurus config with application config
    - moved register autoload to boot method
    - application should in future boot by:
    1. registering autoloader
    2. create config
    3. setting global environment
    4. initialising container with config and environment
    - the environment should be an object that will be injected by the container; current not clear whether container will be able to auto detect environment
    so that it can be injected and is not global static

--- taurus/Application.php
+++ taurus/Application.php
@@ -9,6 +9,7 @@ namespace taurus;
 
 use taurus\framework\Container;
 use taurus\framework\container\TaurusContainerConfig;
+use taurus\framework\db\entity\DatabaseManager;
 use taurus\framework\Environment;
 use taurus\framework\routing\RouteConfig;
 use taurus\framework\routing\Router;
@@ -22,13 +23,35 @@ class Application {
     /** @var Router */
     private $router;
 
-    public function __construct() {
+    /** @var string */
+    private $env;
+
+    public function boot()
+    {
+        $this->registerAutoloader();
         $this->dbManager = Container::getInstance()->getService(TaurusContainerConfig::SERVICE_DB_MANAGER);
+        $this->setEnv();
+        $this->bootConfig();
     }
 
-    public function run() {
-        $this->registerAutoloader();
+    private function setEnv()
+    {
+        if (isset($_ENV[Environment::ENV_VARIABLE_NAME])) {
+            $this->env = $_ENV[Environment::ENV_VARIABLE_NAME];
+        }
 
+        if (isset($_SERVER[Environment::ENV_VARIABLE_NAME])) {
+            $this->env = $_ENV[Environment::ENV_VARIABLE_NAME];
+        }
+    }
+
+    private function bootConfig()
+    {
+
+    }
+
+    public function run()
+    {
         /** @var Request $request */
         $request = Container::getInstance()->getService(TaurusContainerConfig::SERVICE_REQUEST);
         $this->router = Container::getInstance()->getService(TaurusContainerConfig::SERVICE_ROUTER);
