a39658776a5148d7c98e48d021ed1c7fc5463464
mhunger <m.hunger@yahoo.de>
2017-02-12 22:56:53 +0100

    Finish update query
    - added the set method that will take an associative array, because
    it matches what is easily possible with meta data parser
    - where clause that takes a field name and value
    - added an addwhere method that will chain the condition

--- taurus/framework/db/query/UpdateQuery.php
+++ taurus/framework/db/query/UpdateQuery.php
@@ -9,7 +9,66 @@
 namespace taurus\framework\db\query;
 
 
+use taurus\framework\db\query\expression\MultiPartExpression;
+
 class UpdateQuery implements Query
 {
+    /**
+     * @var
+     */
+    private $table;
 
+    /**
+     * @var array
+     */
+    private $updates;
+
+    /** @var array */
+    private $where = [];
+
+    /**
+     * @param string $table
+     * @return UpdateQuery
+     */
+    public function update(string $table): UpdateQuery
+    {
+        $this->table = $table;
+
+        return $this;
+    }
+
+    /**
+     * @param array $updates
+     * @return UpdateQuery
+     */
+    public function set(array $updates): UpdateQuery
+    {
+        $this->updates = $updates;
+
+        return $this;
+    }
+
+    /**
+     * @param $field
+     * @param $value
+     * @return UpdateQuery
+     */
+    public function where($field, $value): UpdateQuery
+    {
+        $this->where[] = [$field => $value];
+
+        return $this;
+    }
+
+    /**
+     * @param $field
+     * @param $value
+     * @return UpdateQuery
+     */
+    public function andWhere($field, $value): UpdateQuery
+    {
+        $this->where[] = [$field => $value];
+
+        return $this;
+    }
 }
