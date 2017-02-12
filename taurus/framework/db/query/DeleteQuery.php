0718bc7902f3f29e66f4e651f38ad76a2391dcb5
mhunger <m.hunger@yahoo.de>
2017-02-12 19:39:38 +0100

    Add table and ids to DeleteQuery
    - added the table and ids to the delete query
    - the ids can be an array
    - Later the delete query will have to be able to do complex filtering

--- taurus/framework/db/query/DeleteQuery.php
+++ taurus/framework/db/query/DeleteQuery.php
@@ -8,8 +8,54 @@
 
 namespace taurus\framework\db\query;
 
-
+/**
+ * Class DeleteQuery
+ * @package taurus\framework\db\query
+ */
 class DeleteQuery implements Query
 {
 
+    /** @var string */
+    private $table;
+
+    /** @var array */
+    private $id;
+
+    /**
+     * @param $table
+     * @return DeleteQuery
+     */
+    public function deleteFrom($table): DeleteQuery
+    {
+        $this->table = $table;
+
+        return $this;
+    }
+
+    /**
+     * @param array $ids
+     * @return DeleteQuery
+     */
+    public function whereId(array $ids): DeleteQuery
+    {
+        $this->id = $ids;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTable(): string
+    {
+        return $this->table;
+    }
+
+    /**
+     * @return array
+     */
+    public function getId(): array
+    {
+        return $this->id;
+    }
 }
