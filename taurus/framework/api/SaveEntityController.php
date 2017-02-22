2667e8fd78c7515e4f44b02afd8825eccc191ec0
mhunger <m.hunger@yahoo.de>
2017-02-22 21:51:59 +0100

    Add controller to save an entity by default
    - added controller that will use save entity service to store an entity from post data
    - the post data has to be in the request in a key that is called like the class
    - Created a test that sets p the controller with service and checks that
    the entity is really stored correctly
    - @TODO: don't use the database schema for fitnessmanager for taurus tests

---
+++ taurus/framework/api/SaveEntityController.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: michaelhunger
+ * Date: 22/02/17
+ * Time: 20:34
+ */
+
+namespace taurus\framework\api;
+
+use taurus\framework\http\Controller;
+use taurus\framework\routing\Request;
+
+class SaveEntityController implements Controller
+{
+    /** @var SaveEntityService */
+    private $service;
+
+    /**
+     * @param Request $request
+     * @return bool
+     */
+    public function handleRequest(Request $request)
+    {
+        return $this->service->saveEntity($request);
+    }
+
+    /**
+     * @param SaveEntityService $service
+     */
+    public function setService(SaveEntityService $service)
+    {
+        $this->service = $service;
+    }
+}
