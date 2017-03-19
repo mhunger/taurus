165a356f7444e24d1ea79e945af19ff4e8df9d0f
mhunger <m.hunger@yahoo.de>
2017-03-19 14:45:42 +0100

    Fix get all entities controller test
    - the get all entities controller test now uses the new method
    of comparing against a json file
    - also included a mock server call rather than instantiated the controller
    directly and calling handling request
    - added a json results set file for loading all the exercises with all the relationships

--- taurus/tests/api/GetAllEntitiesApiControllerTest.php
+++ taurus/tests/api/GetAllEntitiesApiControllerTest.php
@@ -15,6 +15,7 @@ use taurus\framework\api\GetAllEntitiesDefaultServiceImpl;
 use taurus\framework\config\TaurusContainerConfig;
 use taurus\framework\Container;
 use taurus\framework\mock\MockRequest;
+use taurus\framework\mock\MockServer;
 use taurus\tests\AbstractDatabaseTest;
 
 class GetAllEntitiesApiControllerTest extends AbstractDatabaseTest
@@ -25,28 +26,24 @@ class GetAllEntitiesApiControllerTest extends AbstractDatabaseTest
     public function setUp()
     {
         parent::setUp();
-
-        /** @var GetAllEntitiesApiController $controller */
-        $controller = Container::getInstance()->getService(TaurusContainerConfig::SERVICE_DEFAULT_GET_ALL_ENTITIES_CONTROLLER);
-        /** @var GetAllEntitiesDefaultServiceImpl $service */
-        $service = Container::getInstance()->getService(TaurusContainerConfig::SERVICE_DEFAULT_GET_ALL_ENTITIES_SERVICE);
-        $service->setEntityClass(Exercise::class);
-        $controller->setGetAllEntitiesService($service);
-        $this->controller = $controller;
     }
 
     public function testGetAllEntities()
     {
-        $expectedResult = [
-            (new Exercise())->setId(3)->setName('Pull-Up')->setDifficulty('hard')->setVariantName('Chinup'),
-            (new Exercise())->setId(4)->setName('Push-Ups')->setDifficulty('medium')->setVariantName('Standing')
-        ];
+        /** @var MockServer $mockServer */
+        $mockServer = Container::getInstance()
+            ->getService(TaurusContainerConfig::SERVICE_MOCK_SERVER);
 
-        $request = (new MockRequest())->setMethod('GET')->setUrl('test');
-        $this->assertEquals(
-            $expectedResult,
-            $this->controller->handleRequest($request),
-            'Default api controller to load all entities does not return correct entities'
+        $actualResponse = $mockServer->get(
+            '/api/exercises',
+            'GET',
+            []
+        );
+
+        $this->assertJsonStringEqualsJsonFile(
+            $this->getJsonResultsFilePath('GetAllEntitiesApiControllerTest-testGetAllEntities.json'),
+            $actualResponse,
+            'Could not get all resources for exercises through the standard api controller'
         );
     }
 
@@ -56,6 +53,9 @@ class GetAllEntitiesApiControllerTest extends AbstractDatabaseTest
     function getFixtureFiles(): array
     {
         return [
+            'workout_location.xml',
+            'muscle_group.xml',
+            'exercise_group.xml',
             'exercise.xml'
         ];
     }
