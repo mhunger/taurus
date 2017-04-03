83d4cb012c80d5727891103ee66cf75d087576b5
mhunger <m.hunger@yahoo.de>
2017-04-03 20:31:35 +0200

    Create specification interface
    - the specification interface is required by all specification
    - currently it contains the get table method
    - i added that in order to make it explicit which table the specificaiton is for
    - if in annotation, then the table name is just soft, like this
    the constant of the entity can be used
    - that is also the reason, why entities should have constants
    with the table name

---
+++ taurus/framework/db/query/Specification.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: michaelhunger
+ * Date: 02/04/17
+ * Time: 16:37
+ */
+
+namespace taurus\framework\db\query;
+
+/**
+ * Class Specification
+ * @package taurus\framework\db\query
+ *
+ * Parent class for all specifications that are there to represent a specific query
+ * The parent class will ensure all specification are able to return their specs in
+ * normalised form e.g. through expressions
+ *
+ */
+interface Specification
+{
+    /**
+     * Return the table for which this specification is
+     *
+     * @return string
+     */
+    public function getTable(): string;
+}
